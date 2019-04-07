5262d20d898710e73674fc0daf4bb48442abe934
AntonVoronezh <pstut@mail.ru>
2019-04-07 22:26:19 +0300

    add patchs

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const postRouter = require('./routes/post');
+const path = require('path'); 
+const keys = require('./keys'); 
+
+
+
+
+
+
+
+
+
+
