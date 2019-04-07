676c4152f95f32bee09cebfe4c6542bbdcbbc816
AntonVoronezh <pstut@mail.ru>
2019-04-07 22:29:09 +0300

    add GET

--- routes/post.js
+++ routes/post.js
@@ -2,5 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+// http://localhost:5000/api/post  (GET)
+router.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.status(200).json(posts);
+});
+
 
 
