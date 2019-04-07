8a2e26d857f02c3cdf91e99dd34f90b35cf6439b
AntonVoronezh <pstut@mail.ru>
2019-04-07 22:25:21 +0300

    add  keys.js

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://aaa:111@ssssssssssssssssssss-ltwqv.mongodb.net/test?retryWrites=true'
+}
+// https://code.tutsplus.com/ru/tutorials/create-a-database-cluster-in-the-cloud-with-mongodb-atlas--cms-31840
