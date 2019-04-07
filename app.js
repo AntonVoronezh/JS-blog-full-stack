1b1f5cf2fe84b0ca6d5a02ffee351ddbdf611c2d
AntonVoronezh <pstut@mail.ru>
2019-04-07 22:27:22 +0300

    add connect

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const keys = require('./keys');
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
 
+mongoose.connect(keys.mongoURI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err))
 
 
 
