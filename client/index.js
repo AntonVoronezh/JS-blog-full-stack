13d3a38c7596adbd9023eb80e22c4469d9f7e083
AntonVoronezh <pstut@mail.ru>
2019-04-07 22:32:24 +0300

    init

---
+++ client/index.js
@@ -0,0 +1,30 @@
+const card = post => {
+  return `
+    <div class="col s12 m8 offset-m2 l6 offset-l3" id="posts">
+        <div class="card z-depth-4">
+            <div class="card-content">
+                <span class="card-title">${post.title}</span>
+                <p style="white-space: pre-line;">${post.text}</p>
+                <small>${new Date(post.date).toLocaleDateString()}</small>
+            </div>
+            <div class="card-action">
+                <button class="btn btn-small red js-remove" data-id="${post._id}">
+                    <i class="material-icons">delete</i>
+                </button>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+let posts = [];
+let modal;
+const BASE_URL = "/api/post";
+
+
+
+
+
+
+
+
